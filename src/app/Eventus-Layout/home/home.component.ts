a8d83ae815e9478e8a8ab54f983822b22ec9e1cb
Ahmed Sameh <ahmedsaameh14@gmail.com>
2026-04-10 09:25:48 +0200

    Initial Hero Section

--- src/app/Eventus-Layout/home/home.component.ts
+++ src/app/Eventus-Layout/home/home.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-
+  socialLinks = [
+    { name: 'Facebook', url: 'https://www.facebook.com/VEventus/', icon: 'facebook' },
+    { name: 'Instagram', url: 'https://www.instagram.com/eventus_v_/', icon: 'instagram' },
+    { name: 'LinkedIn', url: 'https://www.linkedin.com/company/eventus-v/', icon: 'linkedin' },
+    { name: 'WhatsApp', url: 'https://wa.me/201224489330', icon: 'whatsapp' }
+  ];
 }
